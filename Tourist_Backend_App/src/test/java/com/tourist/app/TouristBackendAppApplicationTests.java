fda0e21ad64696e1c537c293423590e253bfb9e7
unknown <c1julymind58@iiht.tech>
2022-11-15 08:18:56 +0530

    Tourist app

--- Tourist_Backend_App/src/test/java/com/tourist/app/TouristBackendAppApplicationTests.java
+++ Tourist_Backend_App/src/test/java/com/tourist/app/TouristBackendAppApplicationTests.java
@@ -6,8 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +28,7 @@ import antlr.collections.List;
 
 
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TouristBackendAppApplicationTests {
 
 
@@ -33,12 +40,21 @@ TouristServiceImpl tservice;
 @Autowired
 	TouristRepository trepo;
 
+		
 	
 	
+	@BeforeAll
+	 void beforeclass() {
+		System.out.println("Running test cases");
+	}
 	
-	
+	@BeforeEach
+	void beforeeach() {
+		System.out.println("Running before each");
+	}
 	
 	@Test
+	@DisplayName("Create Tourist 1")
 	@Order(1)
 	void testcreate1() {
 		Tourist tourist = new Tourist();
@@ -56,6 +72,7 @@ TouristServiceImpl tservice;
 	}
 
 	@Test
+	@DisplayName("Create Tourist 2")
 	@Order(2)
 	public void testcreate2(){
 		Tourist tourist = new Tourist();
@@ -74,14 +91,16 @@ TouristServiceImpl tservice;
 	
 
 	@Test
+	@DisplayName("Read Tourist 3")
 	@Order(3)
 	public void testRead() {
-		
-		Tourist tourist=trepo.findById(5).get();
+		Tourist tourist = new Tourist();
+		 tourist=trepo.findById(5).get();
 		assertEquals("sam", tourist.getFirstName());
 	}
 	
 	@Test
+	@DisplayName("Update Tourist 4")
 	@Order(4)
 	public void testUpdate() {
 		Tourist tourist = trepo.findById(5).get();
@@ -91,10 +110,21 @@ TouristServiceImpl tservice;
 				}
 	
 	@Test
+	@DisplayName("Delete tourist 5")
 	@Order(5)
 	public void testDelete() {
 		trepo.deleteById(2);
 		assertThat(trepo.existsById(2)).isFalse();
 	}
 
+	@AfterAll
+	void afterall() {
+		System.out.println("Running after test cases");
+	}
+	
+	@AfterEach
+	void aftereach() {
+		System.out.println("Running after each test cases");
+	}
 }
+
